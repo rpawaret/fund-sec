03f6fb0210a7566fb0f65346b3a7e0d6329bb3e8
pawaret <pawaret.r@gmail.com>
2020-01-27 23:52:54 +0700

    add app

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Fund Sec API",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Fund Sec API"),
+        ),
+      ),
+    );
+  }
+}
